245f6b84eb191f493104741b7e3acfb65e2b3ef2
Aizaz <aizaz@192.168.1.4>
2024-02-12 18:20:19 +0500

    [ADD] my hello.py file

---
+++ Hello.py
@@ -0,0 +1,7 @@
+course = "Python for juniors"
+print(course.find("o"))
+print(course.replace("juniors","Absoulte Beginners"))
+print('Python ' in course)
+print(len(course))
+print(course.title())
+course.len()
